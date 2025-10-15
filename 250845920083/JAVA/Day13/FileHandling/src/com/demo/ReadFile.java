ec5a9bb9465d2ac162914687a2e4fc690cf42f7e
Soham Shelar <sohamshelar007@gmail.com>
2025-10-15 12:43:21 +0530

    FILE HANDLING READFILE code

---
+++ 250845920083/JAVA/Day13/FileHandling/src/com/demo/ReadFile.java
@@ -0,0 +1,19 @@
+package com.demo;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
+public class ReadFile {
+  public static void main(String[] args) {
+    File myObj = new File("myfile.txt");
+    try (Scanner myReader = new Scanner(myObj)) {
+      while (myReader.hasNextLine()) {
+        String data = myReader.nextLine();
+        System.out.println(data);
+      }
+    } catch (FileNotFoundException e) {
+      System.out.println("An error occurred.");
+      e.printStackTrace();
+    }
+  }
+}
