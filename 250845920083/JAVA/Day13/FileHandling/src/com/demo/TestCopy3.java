e3803515dff09e116ef00ed32c0162899c234659
Soham Shelar <sohamshelar007@gmail.com>
2025-10-15 12:32:11 +0530

    Day 13 FileHandeling Code

---
+++ 250845920083/JAVA/Day13/FileHandling/src/com/demo/TestCopy3.java
@@ -0,0 +1,28 @@
+package com.demo;
+
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+
+public class TestCopy3 {
+
+	public static void main(String[] args) {
+		
+		//open file in autoclosable resource block
+		try (FileInputStream fis=new FileInputStream("myfile.txt");
+			FileOutputStream fos=new FileOutputStream("myfilecopy.txt");){
+			//copy the contents from one file to another
+			int i=fis.read();
+			//to check the EOF
+			while(i!=-1) {
+				fos.write(i);
+				i=fis.read();
+			}
+			
+		}catch (IOException e) {
+			System.out.println(e.getMessage());
+		}
+
+	}
+
+}
