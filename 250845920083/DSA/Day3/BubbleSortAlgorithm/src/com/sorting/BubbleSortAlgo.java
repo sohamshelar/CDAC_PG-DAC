7e2cadfef99a665ea5faa9cfacdd2453bd4ddec7
ShrutiNitalikar <shrutinitalikar@gmail.com>
2025-10-31 14:44:18 +0530

    DSA Day3 Codes BubbleSort

---
+++ 250845920083/DSA/Day3/BubbleSortAlgorithm/src/com/sorting/BubbleSortAlgo.java
@@ -0,0 +1,51 @@
+package com.sorting;
+
+import java.util.Arrays;
+import java.util.stream.IntStream;
+
+public class BubbleSortAlgo {
+	
+	public static void bubbleSort(int []arr)
+	{
+		for(int i=0;i<arr.length;i++)
+		{
+			for(int j=1;j<arr.length-i;j++)
+			{
+				if(arr[j-1]>arr[j])
+				{
+					int temp=arr[j-1];
+					arr[j-1]=arr[j];
+					arr[j]=temp;
+				}
+			}
+			System.out.println("Iteration : " +i);
+			System.out.println(Arrays.toString(arr));
+		}
+		
+	}
+	
+	public static void ImprovedBubbleSort(int [] arr)
+	{
+		for(int i=0;i<arr.length;i++)
+		{
+			boolean flag=false;
+			for(int j=1;j<arr.length-i;j++)
+			{
+				if(arr[j-1]>arr[j])
+				{
+					flag=true;
+					int temp=arr[j-1];
+					arr[j-1]=arr[j];
+					arr[j]=temp;
+				}
+			}
+			System.out.println("Iteration : "+ i);
+			System.out.println(Arrays.toString(arr));
+			if(!flag)
+			{
+				break;
+			}
+		}
+	}
+
+}
---
+++ 250845920083/DSA/Day3/BubbleSortAlgorithm/src/com/test/TestBubbleSort.java
@@ -0,0 +1,17 @@
+package com.test;
+
+import java.util.stream.IntStream;
+
+import com.sorting.BubbleSortAlgo;
+
+public class TestBubbleSort {
+
+	public static void main(String[] args) {
+		int [] arr= {10,2,34,3,53,433,4,22,33,88};
+		//BubbleSortAlgo.bubbleSort(arr);
+		BubbleSortAlgo.ImprovedBubbleSort(arr);
+		IntStream.of(arr).forEach(e->System.out.println(e +"\t"));
+
+	}
+
+}
