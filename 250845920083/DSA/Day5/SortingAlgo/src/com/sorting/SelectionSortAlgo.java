d4b699e2f96a8a76b72580aa00897b53f50f8331
sohamshelar <sohamshelar007@gmail.com>
2025-11-03 16:42:16 +0530

    DSA Day 5 Selection Sort

--- 250845920083/DSA/Day5/SortingAlgo/src/com/sorting/SelectionSortAlgo.java
+++ 250845920083/DSA/Day5/SortingAlgo/src/com/sorting/SelectionSortAlgo.java
@@ -1,5 +1,58 @@
 package com.sorting;
 
+import java.util.Arrays;
+
 public class SelectionSortAlgo {
+	private static int findNthMin(int[] arr, int start) {
+		int minpos=start;
+		int min=arr[minpos];
+		for(int i=start;i<arr.length;i++) {
+			if(min>arr[i]) {
+				minpos=i;
+				min=arr[i];
+			}
+		}
+		
+		return minpos;
+	}
+	public static void selectionSortAscending(int[] arr) {
+		for(int i=0;i<arr.length-1;i++) {
+			
+			int minpos=findNthMin(arr,i);
+		
+			int temp=arr[i];
+			arr[i]=arr[minpos];
+			arr[minpos]=temp;
+			System.out.println(Arrays.toString(arr));
+			
+		}
+		
+	}
+	
+	private static int findNthMax(int[] arr, int start) {
+		int maxpos=start;
+		int max=arr[maxpos];
+		for(int i=start;i<arr.length;i++) {
+			if(max<arr[i]) {
+				maxpos=i;
+				max=arr[i];
+			}
+		}
+		
+		return maxpos;
+	}
+	public static void selectionSortDescending(int[] arr) {
+		for(int i=0;i<arr.length-1;i++) {
+			
+			int maxpos=findNthMax(arr,i);
+			
+			int temp=arr[i];
+			arr[i]=arr[maxpos];
+			arr[maxpos]=temp;
+			System.out.println(Arrays.toString(arr));
+			
+		}
+		
+	}
 
 }
---
+++ 250845920083/DSA/Day5/SortingAlgo/src/com/test/TestSelectionSort.java
@@ -0,0 +1,14 @@
+package com.test;
+
+import com.sorting.SelectionSortAlgo;
+
+public class TestSelectionSort {
+
+	public static void main(String[] args) {
+		int[] arr= {15,13,30,20,5,7,21,2};
+		//SelectionSortAlgo.selectionSortAscending(arr);
+		SelectionSortAlgo.selectionSortDescending(arr);
+
+	}
+
+}
