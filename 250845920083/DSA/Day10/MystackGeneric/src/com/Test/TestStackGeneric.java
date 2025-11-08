6c6355a9784a992b2f1ec82719d158c1b26d847b
sohamshelar <sohamshelar007@gmail.com>
2025-11-08 20:00:59 +0530

    DSA Day11 Code BinaryTree

--- 250845920083/DSA/Day10/MystackGeneric/src/com/Test/TestStackGeneric.java
+++ 250845920083/DSA/Day10/MystackGeneric/src/com/Test/TestStackGeneric.java
@@ -5,13 +5,19 @@ import com.Stack.MyStackGeneric;
 public class TestStackGeneric {
 	public static void main(String[] args) {
 		MyStackGeneric ob=new MyStackGeneric(5);
+		
+		
 		  ob.push(25);
 		  ob.push(202);
 		  ob.push(20);
 		  ob.push(7);
 		  ob.push(350);
 		  ob.push(23);
+		  
+		  
 		  System.out.println("-------------------");
+		  
+		  
 		  System.out.println(ob.pop());
 		  System.out.println(ob.pop());
 		  System.out.println(ob.pop());
@@ -19,6 +25,4 @@ public class TestStackGeneric {
 		  System.out.println(ob.pop());
 		  System.out.println(ob.pop());
 	}
-  
-  
 }
---
+++ 250845920083/DSA/Day11/MyBinarySearchTREE/src/com/demo/test/TestBinarySearchTree.java
@@ -0,0 +1,42 @@
+package com.demo.test;
+
+import com.demo.trees.MyBinarySearchTree;
+
+public class TestBinarySearchTree 
+{
+
+	public static void main(String[] args)
+	{
+		MyBinarySearchTree bst=new MyBinarySearchTree();
+		
+		bst.insertNode(35);
+		bst.insertNode(155);
+		bst.insertNode(177);
+		bst.insertNode(185);
+		bst.insertNode(105);
+		bst.insertNode(401);
+		bst.insertNode(328);
+		bst.insertNode(455);
+		bst.insertNode(333);
+		
+		System.out.println("Inorder");
+		bst.inorder();
+		System.out.println("preorder");
+		bst.preorder();
+		System.out.println("postorder");
+		bst.postorder();
+		
+		bst.search(33);
+		bst.searchNonRecussive(333);
+		bst.search(38);
+		
+		bst.deleteNode(177);
+		bst.inorder();
+		bst.deleteNode(105);
+		bst.inorder();
+		bst.deleteNode(328);
+		bst.inorder();
+
+	}
+
+}
