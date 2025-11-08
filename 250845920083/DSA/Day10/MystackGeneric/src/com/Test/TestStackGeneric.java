525da161665a889f9366fb2c4e4022ca34c5b379
sohamshelar <sohamshelar007@gmail.com>
2025-11-08 19:37:58 +0530

    DSA Day10 Code StackGeneric

---
+++ 250845920083/DSA/Day10/MystackGeneric/src/com/Test/TestStackGeneric.java
@@ -0,0 +1,24 @@
+package com.Test;
+
+import com.Stack.MyStackGeneric;
+
+public class TestStackGeneric {
+	public static void main(String[] args) {
+		MyStackGeneric ob=new MyStackGeneric(5);
+		  ob.push(25);
+		  ob.push(202);
+		  ob.push(20);
+		  ob.push(7);
+		  ob.push(350);
+		  ob.push(23);
+		  System.out.println("-------------------");
+		  System.out.println(ob.pop());
+		  System.out.println(ob.pop());
+		  System.out.println(ob.pop());
+		  System.out.println(ob.pop());
+		  System.out.println(ob.pop());
+		  System.out.println(ob.pop());
+	}
+  
+  
+}
