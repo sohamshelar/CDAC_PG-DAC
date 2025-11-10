8f6859f3857f37a602022b75a6ea74d8ddd5a107
sohamshelar <sohamshelar007@gmail.com>
2025-11-10 14:40:09 +0530

    All data

--- 250845920083/DSA/Day11/MyHashTable/src/com/Test/TestMyHashTable.java
+++ 250845920083/DSA/Day11/MyHashTable/src/com/Test/TestMyHashTable.java
@@ -1,10 +1,27 @@
 package com.Test;
 
+import com.Hashing.MyHashTable;
+
 public class TestMyHashTable {
 
-	public static void main (string[] args)
+	public static void main (String[] args)
 	{
-		MyHash
+		MyHashTable ht=new MyHashTable(5);
+		ht.insertData(41);
+		ht.insertData(48);
+		ht.insertData(6);
+		ht.insertData(4);
+		ht.insertData(8);
+		ht.insertData(54);
+		ht.insertData(85);
+		ht.insertData(79);
+		ht.insertData(56);
+		ht.insertData(81);
+		ht.insertData(94);
+		ht.insertData(83);
+		ht.displayHashTable();
+		ht.searchData(79);
+		ht.searchData(1001);
 	}
 
 }
