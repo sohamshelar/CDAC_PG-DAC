c48b5d272c8af10fe511e307adc960fd9771c593
sohamshelar <sohamshelar007@gmail.com>
2025-12-09 18:57:29 +0530

    Advance java day17 notes spring boot

--- 250845920083/MySpringWebserviceProductCrud/src/main/java/com/demo/dao/LoginDao.java
+++
@@ -1,9 +0,0 @@
-package com.demo.dao;
-
-import com.demo.beans.MyUSer;
-
-public interface LoginDao {
-
-	MyUSer autheticateUser(String uname, String passwd);
-
-}
--- 250845920083/MySpringWebserviceProductCrud/src/main/java/com/demo/service/LoginService.java
+++
@@ -1,9 +0,0 @@
-package com.demo.service;
-
-import com.demo.beans.MyUSer;
-
-public interface LoginService {
-
-	MyUSer validateUser(String uname, String passwd);
-
-}
