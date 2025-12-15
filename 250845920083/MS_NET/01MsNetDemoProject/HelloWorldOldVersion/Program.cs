279544cc1e735fdfc41dbcfc6999e1203d8cf4c9
sohamshelar <sohamshelar007@gmail.com>
2025-12-15 18:20:18 +0530

    Day 1 Ms.net code

---
+++ 250845920083/MS_NET/01MsNetDemoProject/HelloWorldOldVersion/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HelloWorldOldVersion
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("hello world \n");
+
+            Console.Write("Enter your name \n");
+            String name=Console.ReadLine();
+
+            Console.WriteLine($"Hello ,{name}");
+           
+
+            int a = 5;
+            Console.Write("Table of " + a);
+
+            for (int i=1;i<=10;i++)
+            {
+                int c = a * i;
+                Console.Write("\n"+c);
+            }
+        }
+    }
+}
