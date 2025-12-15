5eca0a039280e215c726b65b347e52c4269908eb
sohamshelar <sohamshelar007@gmail.com>
2025-12-15 19:28:52 +0530

    Day 1 Ms.net code pattern

---
+++ 250845920083/MS_NET/01MsNetDemoProject/Pattern1/Program.cs
@@ -0,0 +1,54 @@
+﻿namespace Pattern1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = 4;
+
+            for (int i = 1; i <= n; i++)
+            {
+                for (int s = 1; s <= n - i; s++)
+                {
+                    Console.Write(" ");
+                }
+
+                
+                Console.Write("*");
+                for (int j = 2; j < 2 * i - 1; j++)  
+                {
+                    Console.Write(" ");
+                }
+                if (2 * i - 1 > 1)  
+                {
+                    Console.Write("*");
+                }
+                Console.WriteLine();
+            }
+
+
+            for (int i = n - 1; i >= 1; i--)
+            {
+
+                for (int s = 1; s <= n - i; s++)
+                {
+                    Console.Write(" ");
+                }
+
+
+                Console.Write("*");
+                for (int s1 = 2; s1 < 2 * i - 1; s1++)
+                {
+                    Console.Write(" ");
+                }
+                if (2 * i - 1 > 1)
+                {
+                    Console.Write("*");
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
+
+
