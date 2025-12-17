50d6e643ae9e4dde3d0889c34fa2857914307717
sohamshelar <sohamshelar007@gmail.com>
2025-12-17 16:57:26 +0530

    Ms.Net Seal Class Concept

---
+++ 250845920083/MS_NET/Day2/SealClass/Program.cs
@@ -0,0 +1,49 @@
+﻿namespace SealClass
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Code ........");
+        }
+    }
+    public sealed class CMath
+    {
+        public int Add(int x, int y)
+        {
+            return x + y;
+        }
+        public int Sub(int x, int y)
+        {
+            return x - y;
+        }
+    }
+
+    public class AdvanceMath
+    {
+        public virtual int Square(int x)
+        {
+            return x * x;
+        }
+    }
+
+    public class MyMath : AdvanceMath
+    {
+        public virtual void SayHello()
+        {
+            Console.WriteLine("Hello Everyone !");
+        }
+        public sealed override int Square(int x)
+        {
+            return x * x * 100;
+        }
+    }
+
+    public class MyMath2 : MyMath
+    {
+        public override void SayHello()
+        {
+            Console.WriteLine("Hi Shashank and Soham ");
+        }
+    }
+}
