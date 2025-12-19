e80c741b557bb1419f29c0b2ccc55a4d64e943e0
sohamshelar <sohamshelar007@gmail.com>
2025-12-19 19:56:50 +0530

    MulticastDelegate code

---
+++ 250845920083/MS_NET/DelegateAndEvent/MultiCastDelegate/Program.cs
@@ -0,0 +1,62 @@
+﻿namespace MultiCastDelegate
+{
+    public delegate void MyEventHandler();
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Is Guest arrived");
+            string isGuestArrived = Console.ReadLine();
+
+            CollegeAnnualEvent obj = new CollegeAnnualEvent();
+            MyEventHandler handler = obj.WelCome;
+            handler += obj.WelSpeech;
+            if (isGuestArrived == "y")
+            {
+                handler += obj.GuestSpeech;
+            }
+            else
+            {
+                handler -= obj.GuestSpeech;
+            }
+            handler += obj.Dance;
+            handler += obj.Dinner;
+            handler += obj.GoodBye;
+            handler += obj.EndEvent;
+
+            handler();
+        }
+    }
+
+    public class CollegeAnnualEvent
+    {
+        public void WelCome()
+        {
+            Console.WriteLine("Welcome Everyone!!");
+        }
+        public void WelSpeech()
+        {
+            Console.WriteLine("Host:good morning everyone!!");
+        }
+        public void GuestSpeech()
+        {
+            Console.WriteLine("Guest: hello to one & all !!");
+        }
+        public void Dance()
+        {
+            Console.WriteLine("Oh GOD, we have have to go through this!!");
+        }
+        public void Dinner()
+        {
+            Console.WriteLine("We insist everyone , please enjoy dinner...!!");
+        }
+        public void GoodBye()
+        {
+            Console.WriteLine("Bye bye.. please ghar jao...!!");
+        }
+        public void EndEvent()
+        {
+            Console.WriteLine("Pack UP...!!");
+        }
+    }
+}
