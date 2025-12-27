44e38a475af040f931c8f85194d659a38f2ca8e1
sohamshelar <sohamshelar007@gmail.com>
2025-12-27 19:52:55 +0530

    Database connection code Ms.net

---
+++ 250845920083/MS_NET/CSharpDemos/ConnectedAdoNet/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace ConnectedAdoNet
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string connectionString = "Data Source=(LocalDB)\\MSSQLLocalDB;Initial Catalog=Dac83;Integrated Security=True";
+        }
+    }
+}
