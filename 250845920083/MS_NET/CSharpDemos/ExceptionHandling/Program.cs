e1c721fa54aafbcd0021f468f50241f1723f941b
sohamshelar <sohamshelar007@gmail.com>
2025-12-30 14:53:00 +0530

    Exception Handling

---
+++ 250845920083/MS_NET/CSharpDemos/ExceptionHandling/Program.cs
@@ -0,0 +1,38 @@
+﻿namespace ExceptionHandling
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                Console.WriteLine("Enter first No :");
+                int n1 = Convert.ToInt32(Console.ReadLine());
+
+                Console.WriteLine("Enter second no :");
+                int n2 = Convert.ToInt32(Console.ReadLine());
+
+                if (n2 == 0)
+                {
+                    throw new MyCustomException("do not enter second number zero");
+
+                }
+                int divResult = n1 / n2;
+                Console.WriteLine($"Div Result: = {divResult}");
+            }
+            catch (MyCustomException obj)
+            {
+                Console.WriteLine(obj.Message);
+            }
+        }
+    }
+    public class MyCustomException : Exception
+    {
+        public string MyMessage { get; set; }
+
+        public MyCustomException(string msg)
+        {
+            MyMessage = msg;
+        }
+    }
+}
